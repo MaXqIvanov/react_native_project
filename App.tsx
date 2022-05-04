a0b7105855bdeedc8d96f44df3e29816814def1c
MaXqIvanov <maksivanov35@ya.ru>
2022-05-04 09:59:31 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert,Image } from 'react-native';
 
 export default function App() {
+  const handleOnButton = (props:any)=>{
+    Alert.alert('Choise anything', 'anyMessage?', [
+      {text:'yes',onPress:()=>console.log("heu")
+      },
+      {text:'close'}
+    ])
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={s.container}>
+      <View style={s.container_wrap}>
+        <Text style={s.text}>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <View style={s.mainButton}>
+            <Button  onPress={()=> handleOnButton("hello")} title={'put on me'} />
+        </View>
+        
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgb(26,34,43)',
   },
+  container_wrap:{
+    paddingTop: 25,
+    height: 100+'%',
+    width: 100+'%',
+  },
+  text:{
+    width: 100+'%',
+    color: 'white',
+    textAlign: 'center'
+  },
+  mainButton:{
+      flex: 1,
+      justifyContent: 'flex-end',
+      width: 100+'%',
+  }
 });
