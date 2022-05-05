a3eae6c8e1ab1b9bf025ae9bd1db366f7f3bbbc1
MaXqIvanov <maksivanov35@ya.ru>
2022-05-05 15:25:29 +0300

    update

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Alert,Image } from 'react-native';
+import MainPages from './pages/MainPages';
 
 export default function App() {
-  const handleOnButton = (props:any)=>{
-    Alert.alert('Choise anything', 'anyMessage?', [
-      {text:'yes',onPress:()=>console.log("heu")
-      },
-      {text:'close'}
-    ])
-  }
+  
   return (
     <View style={s.container}>
       <View style={s.container_wrap}>
-        <Text style={s.text}>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <View style={s.mainButton}>
-            <Button  onPress={()=> handleOnButton("hello")} title={'put on me'} />
-        </View>
-        
+        <MainPages />
       </View>
     </View>
   );
@@ -29,7 +19,6 @@ const s = StyleSheet.create({
     backgroundColor: 'rgb(26,34,43)',
   },
   container_wrap:{
-    paddingTop: 25,
     height: 100+'%',
     width: 100+'%',
   },
